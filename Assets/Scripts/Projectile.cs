a3e1b15b16a767f71d2c90f1ba97696c3d55b828
Sara López Llopis <al416577@uji.es>
2021-12-16 14:33:00 +0100

    trabajo finalizado

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    public float targetTime = 10.0f;
+
+
+        void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+
+    
+}
